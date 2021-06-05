7e2ebaf0652a7c3736591d338831c61318119f17
Juan Cerezo <juan-cristobal.cerezo@gft.com>
2021-06-05 11:54:55 +0200

    first commit

---
+++ web-image-store/src/main/java/org/decibel/services/imagestore/WebImageStoreApplication.java
@@ -0,0 +1,13 @@
+package org.decibel.services.imagestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebImageStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebImageStoreApplication.class, args);
+	}
+
+}
---
+++ web-image-store/src/test/java/org/decibel/services/imagestore/WebImageStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.decibel.services.imagestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebImageStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
